a42b02245c8fd548f4964f4668405de63a00fe1c
Sudhi Ranjan Gupta <sudhigupta190@gmail.com>
2022-09-24 20:48:06 +0530

    Now Fine

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 export const connectDatabase = () =>{
-    mongoose.connect(process.env.MONGO_URI || "mongodb+srv://slasher190:1234@cluster0.5awar.mongodb.net/portfolio?retryWrites=true&w=majority").then((c) => {
+    mongoose.connect(process.env.MONGO_URI || "mongodb://slasher190:1234@cluster0.5awar.mongodb.net/portfolio?retryWrites=true&w=majority").then((c) => {
         console.log(`MongoDB connected to: ${c.connection.host}`);
     }).catch((e) => {
         console.log(e);
